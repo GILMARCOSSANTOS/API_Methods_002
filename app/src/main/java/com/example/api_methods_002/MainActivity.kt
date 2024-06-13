748c7a16b9b7f40546ba3e40ea750ff1c3168340
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2024-06-13 01:38:33 -0300

    Salvar ViewBinding

--- app/src/main/java/com/example/api_methods_002/MainActivity.kt
+++ app/src/main/java/com/example/api_methods_002/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.api_methods_002.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /* Global Escope Variables */
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
