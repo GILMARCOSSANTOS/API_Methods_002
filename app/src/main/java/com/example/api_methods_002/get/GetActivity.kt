83da22fd88f45b472189f7707f7466962c744b87
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2024-06-15 12:20:06 -0300

    Salvar

--- app/src/main/java/com/example/api_methods_002/get/GetActivity.kt
+++ app/src/main/java/com/example/api_methods_002/get/GetActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.api_methods_002.R
 import com.example.api_methods_002.databinding.ActivityGetBinding
+import com.google.android.material.textview.MaterialTextView
 
 class GetActivity : AppCompatActivity() {
 
@@ -24,5 +25,14 @@ class GetActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        /* Execution of Functions */
+        globalLevel()
+    }
+
+    private fun globalLevel (){
+
+        val textViewTitle = findViewById<MaterialTextView>(R.id.textView_screenTitle_id)
+        textViewTitle.text = getString(R.string.get_method)
     }
 }
